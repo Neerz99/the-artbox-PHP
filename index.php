165624ec648027d7ecd03c8b9e8de42a1d5ce991
Neerz99 <iloenzo99@gmail.com>
2025-02-16 19:17:40 +0100

    added .gitignore

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <article class="oeuvre">
             <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
                 <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
-                <h2><?= $oeuvre['titre'] ?></h2>
+                <h2><?= stripcslashes($oeuvre['titre']) ?></h2>
                 <p class="description"><?= $oeuvre['artiste'] ?></p>
             </a>
         </article>
